a14732690145aa86697e64107f71f6a27674b3b3
chandugitg <muvvala99666@gmail.com>
2022-01-27 21:18:50 +0530

    added something in test file

---
+++ test.js
@@ -0,0 +1,15 @@
+// local and remote repo s
+
+// git checkout "random commit"
+
+// git log
+
+//branches
+
+
+//we are doing something in newbranch now
+
+//we will test some new features
+
+
+//skbjs
